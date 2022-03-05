19c9d32ff531c01f34bf6eb70ee5c4403657248b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-03-05 12:41:08 -0900

    update trigger

--- Components/RepositoryObject/RepositoryTrigger.cs
+++ Components/RepositoryObject/RepositoryTrigger.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class RepositoryTrigger : MonoBehaviour
 {
-    public string url;
+    public string repoUrl;
+    public string pagesUrl;
     JavaScriptUtility jsUtility;
+    [SerializeField]
+    private bool _occupied = false;
     void Start()
     {
         jsUtility = FindObjectOfType<JavaScriptUtility>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Repository") && _occupied)
+        {
+            OpenLink(repoUrl);
+        }
+        if(Input.GetButtonDown("Pages") && _occupied && pagesUrl != "")
+        {
+            OpenLink(pagesUrl);
+        }
     }
+
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.tag == "Player"){
-            Debug.Log(url);
-            jsUtility.SendToJs(url);
+        if(collider.tag == "Player")
+        {
+        _occupied = true;
         }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            _occupied = false;
+        }
+    }
+    void OpenLink(string url)
+    {
+        Debug.Log(url);
+        jsUtility.SendToJs(url);
     }
 }
